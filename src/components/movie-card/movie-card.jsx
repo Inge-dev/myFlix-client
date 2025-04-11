328e2113e71435d50dd1e0160f6d3ecc1d069d05
Inge-dev <ingelewerentz@gmail.com>
2025-04-10 19:21:54 -0700

    Fetch movies from API and update components with PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,24 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string // or PropTypes.instanceOf(Date) if you parse it
+    }),
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,44 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.ImagePath.trim()} alt={movie.Title} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre?.Name}</span>
+      </div>
+      <div>
+        <span>Genre Description: </span>
+        <span>{movie.Genre?.Description}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.Director?.Name}</span>
+      </div>
+      <div>
+        <span>Director Bio: </span>
+        <span>{movie.Director?.Bio}</span>
+      </div>
+      <div>
+        <span>Birth Date: </span>
+        <span>{new Date(movie.Director?.Birth).toLocaleDateString()}</span>
+      </div>
+      <div>
+        <span>Featured: </span>
+        <span>{movie.Featured ? 'Yes' : 'No'}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
-};
+};   
