75ecef40ce3cdc9a7a15d94442064fdd138b7ede
Inge-dev <ingelewerentz@gmail.com>
2025-04-15 12:32:43 -0700

    Update main-view.jsx
    Updated MainView component to initialize user and token state from storedUser if available. This change allows for persisting user and token values accross sessions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -17,9 +17,9 @@ export const MainView = () => {
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(storedUser ? storedUser : null);
 
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(storedUser ? storedUser : null);
 
   useEffect(() => {
     if (!token) {
