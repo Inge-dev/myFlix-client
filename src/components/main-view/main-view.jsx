6e6c528a2e87d2ab0ba3e8b5c612eccf2bfc8582
Inge-dev <ingelewerentz@gmail.com>
2025-04-14 18:10:15 -0700

    Update main-view.jsx
    Imported LoginView and SignupView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,15 +4,34 @@ import { MovieCard } from "../movie-card/movie-card.jsx";
 
 import { MovieView } from "../movie-view/movie-view.jsx";
 
+import { LoginView } from "../login-view/login-view.jsx";
+
+import { SignupView } from "../signup-view/signup-view.jsx";
+
+
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+
   const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  const [user, setUser] = useState(null);
+
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    fetch("https://movie-app-il-c396ba198e0e.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://movie-app-il-c396ba198e0e.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
-      .then((data) => {
+      .then((movies) => {
+        // console.log(movies);
         // const moviesFromApi = data.map((movie) => {
         //   return {
         //     id: movie._id,
@@ -24,11 +43,30 @@ export const MainView = () => {
         //     featured: movie.Featured,
         //   };
         // });
-        setMovies(data);
+        setMovies(movies);
       });
-  }, []);
+  }, [token]);
 
-  console.log("movies:", movies);
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView
+          onSignedUp={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+      </>
+    );
+  }
+
 
   if (selectedMovie) {
     return (
@@ -42,16 +80,19 @@ export const MainView = () => {
 
   return (
     <div>
-      <h2>Movie List</h2>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie._id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
+      <div>
+        <h2>Movie List</h2>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie._id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+      </div>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     </div>
   );
 };
